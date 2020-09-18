1da7da6f8f3e077a400f2f9b2ff488115651d390
petros <pit.trak@gmail.com>
2020-09-18 21:16:10 +0300

    GraphQL scalar types

--- src/index.js
+++ src/index.js
@@ -1,29 +1,35 @@
 import { GraphQLServer } from 'graphql-yoga'
 
+// Scalar types of GraphQL: String, Boolean, , Int, Float, ID
+
 // Type definitions (schema)
 const typeDefs = `
     type Query {
-        hello: String!
-        name: String!
-        location: String!
-        bio: String!
+        title: String!
+        price: Float!
+        releaseYear: Int
+        rating: Float
+        inStock: Boolean!
     }
 `
 
 // Resolvers
 const resolvers = {
     Query: {
-        hello() {
-            return 'This is my first query!'
+        title() {
+            return 'Blue Jeans'
         },
-        name() {
-            return 'My name is Petros'
+        price() {
+            return 45.99
         },
-        location() {
-            return 'I leave in Athens, Greece'
+        releaseYear() {
+            return 2019
         },
-        bio() {
-            return 'I am an entry level full stack developer'
+        rating() {
+            return 4.5
+        },
+        inStock() {
+            return true
         }
     }
 }
@@ -33,6 +39,7 @@ const server = new GraphQLServer({
     resolvers
 })
 
+// visit localhost:4000 to do the queries
 server.start(() => {
     console.log('The server is up');
 })
