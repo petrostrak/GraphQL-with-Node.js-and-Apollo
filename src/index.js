6a56649ec11873efb75088cfb068e4bd98cf6d95
petros <pit.trak@gmail.com>
2020-09-18 21:02:16 +0300

    Created sample GraphQL API

--- src/index.js
+++ src/index.js
@@ -1,1 +1,38 @@
-console.log('Hello GraphQL');
+import { GraphQLServer } from 'graphql-yoga'
+
+// Type definitions (schema)
+const typeDefs = `
+    type Query {
+        hello: String!
+        name: String!
+        location: String!
+        bio: String!
+    }
+`
+
+// Resolvers
+const resolvers = {
+    Query: {
+        hello() {
+            return 'This is my first query!'
+        },
+        name() {
+            return 'My name is Petros'
+        },
+        location() {
+            return 'I leave in Athens, Greece'
+        },
+        bio() {
+            return 'I am an entry level full stack developer'
+        }
+    }
+}
+
+const server = new GraphQLServer({
+    typeDefs,
+    resolvers
+})
+
+server.start(() => {
+    console.log('The server is up');
+})
