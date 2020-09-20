9207314740d19cb2531a875f2f716b43fda732a5
petros <pit.trak@gmail.com>
2020-09-20 16:34:25 +0300

    Created data with mutations

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import { GraphQLServer } from 'graphql-yoga'
+import {v4 as uuid} from "uuid"
 
 // Scalar types of GraphQL: String, Boolean, , Int, Float, ID
 
@@ -79,6 +80,10 @@ const typeDefs = `
         post: Post!
     }
 
+    type Mutation {
+        createUser(name: String!, email: String!, age: Int): User!
+    }
+
     type User {
         id: ID!
         name: String!
@@ -147,6 +152,25 @@ const resolvers = {
                 body: 'This is a body of a post',
                 isPublished: true
             }
+        }
+    },
+    Mutation: {
+        createUser(parent, args, ctx,info) {
+            const emailTaken = users.some((user) => user.email === args.email)
+
+            if(emailTaken) {
+                throw new Error('Email taken')
+            }
+
+            const user = {
+                id: uuid(),
+                name: args.name,
+                email: args.email,
+                age: args.age
+            }
+
+            users.push(user)
+            return user
         }
     },
     Post: {
