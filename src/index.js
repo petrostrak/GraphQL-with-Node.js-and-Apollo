2ed56b1b999c33de0d41d81c06659ad05f79164a
petros <pit.trak@gmail.com>
2020-09-20 22:19:11 +0300

    Deleting data with Mutations (user with posts and comments)

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import {v4 as uuid} from 'uuid'
 // Scalar types - String, Boolean, Int, Float, ID
 
 // Demo user data
-const users = [{
+let users = [{
     id: '1',
     name: 'Andrew',
     email: 'andrew@example.com',
@@ -20,7 +20,7 @@ const users = [{
 }]
 
 // Demo post data
-const posts = [{
+let posts = [{
     id: '10',
     title: 'GraphQL 101',
     body: 'This is how to use GraphQL...',
@@ -40,7 +40,7 @@ const posts = [{
     author: '2'
 }]
 // Demo comments data
-const comments = [{
+let comments = [{
     id: '102',
     text: 'This worked well for me. Thanks!',
     author: '3',
@@ -74,6 +74,7 @@ const typeDefs = `
 
     type Mutation {
         createUser(data: CreateUserInput!): User!
+        deleteUser(id: ID!): User!
         createPost(data: CreatePostInput!): Post!
         createComment(data: CreateCommentInput!): Comment!
     }
@@ -181,6 +182,29 @@ const resolvers = {
             users.push(user)
 
             return user
+        },
+        deleteUser(parent, args, ctx, info) {
+            const userIndex = users.findIndex((user) => user.id === args.id)
+
+            if(userIndex === -1) {
+                throw new Error('User not found')
+            }
+
+            const deletedUsers = users.splice(userIndex, 1)
+
+            posts = posts.filter((post) => {
+                const match = post.author === args.id
+
+                if(match) {
+                    comments = comments.filter((comment) => comment.post !== post.id)
+                }
+
+                return !match
+            })
+
+            comments = comments.filter((comment) => comment.author !== args.id)
+
+            return deletedUsers[0]
         },
         createPost(parent, args, ctx, info) {
             const userExists = users.some((user) => user.id === args.data.author)
