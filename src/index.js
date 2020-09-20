a39325c3a88967ce934d20557b2671ab80ba6b2a
petros <pit.trak@gmail.com>
2020-09-20 22:45:36 +0300

    Deleting data with Mutations (posts and comments)

--- src/index.js
+++ src/index.js
@@ -76,6 +76,7 @@ const typeDefs = `
         createUser(data: CreateUserInput!): User!
         deleteUser(id: ID!): User!
         createPost(data: CreatePostInput!): Post!
+        deletePost(id: ID!): User!
         createComment(data: CreateCommentInput!): Comment!
     }
 
@@ -203,7 +204,6 @@ const resolvers = {
             })
 
             comments = comments.filter((comment) => comment.author !== args.id)
-
             return deletedUsers[0]
         },
         createPost(parent, args, ctx, info) {
@@ -222,6 +222,17 @@ const resolvers = {
 
             return post
         },
+        deletePost(parent, args, ctx, info) {
+            const postIndex = posts.findIndex((post) => post.id === args.id)
+
+            if(postExists === -1) {
+                throw new Error('Post not found')
+            }
+
+            const deletedPost = posts.splice(postIndex, 1)
+            comments = comments.filter((comment) => comment.post !== args.id)
+            return deletedPost
+        },
         createComment(parent, args, ctx, info){
             const userExists = users.some((user) => user.id === args.data.author)
             const postExists = posts.some((post) => post.id === args.data.post && post.published)
@@ -238,6 +249,16 @@ const resolvers = {
             comments.push(comment)
 
             return comment
+        },
+        deleteComment(parent, args, ctx, info) {
+            const commentIndex = comments.findIndex((comment) => comment.id === args.id)
+
+            if(commentIndex === -1) {
+                throw new Error('Comment not found')
+            }
+
+            const deletedComment = comments.splice(commentIndex, 1)
+            return deletedComment[0]
         }
     },
     Post: {
