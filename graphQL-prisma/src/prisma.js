a550cc26d9ea5de0b519201c8c022d2486c5eb43
petros <pit.trak@gmail.com>
2020-09-24 20:06:33 +0300

    Mutations with prisma bindings

--- graphQL-prisma/src/prisma.js
+++ graphQL-prisma/src/prisma.js
@@ -6,11 +6,48 @@ const prisma = new Prisma({
 })
 
 // prisma.query prisma.mutation prisma.subscription prisma.exists
+// These methods take two arguments. The 1st is the operation arguments
+// and the 2nd is the selection set
 
-prisma.query.users(null, '{ id name email posts { id title } comments { id text } }')
-    .then((data) => console.log(JSON.stringify(data, undefined, 2)))
-    .catch((err) =>  console.log(err))
+// // prisma.query
+// prisma.query.users(null, '{ id name email posts { id title body } comments { id text } }')
+//     .then((data) => console.log(JSON.stringify(data, undefined, 2)))
+//     .catch((err) =>  console.log(err))
 
-prisma.query.comments(null, '{ id text author { id name } }')
+// prisma.query.comments(null, '{ id text author { id name } }')
+//     .then((data) => console.log(JSON.stringify(data, undefined, 2)))
+//     .catch((err) => console.log(err))
+
+// // prisma.mutation
+// prisma.mutation.createPost({
+//     data: {
+//         title: "GraphQL!!",
+//         body: "",
+//         published: false,
+//         author: {
+//             connect: {
+//                 id: "ckfh0zvn700uj07087vijilrt"
+//             }
+//         }
+//     }
+// }, '{ id title body published }')
+//     .then((data) => {
+//         console.log(data)
+//         return prisma.query.users(null, '{ id name email posts { id title body }}')
+//     })
+//     .then((data) => console.log(JSON.stringify(data, undefined, 2)))
+
+prisma.mutation.updatePost({
+    data: {
+        body: "Trying to mess around with mutations via Node.js",
+        published: true
+    },
+    where: {
+        id: "ckfh24w7801mw0708i3c6862c"
+    }
+}, '{ id title body published}')
+    .then((data) => {
+        console.log(JSON.stringify(data, undefined, 2))
+        return prisma.query.posts(null, '{ id title body published }')
+    })
     .then((data) => console.log(JSON.stringify(data, undefined, 2)))
-    .catch((err) => console.log(err))
