37667905900a8675f2c6747c686aefca56cf04c0
petros <pit.trak@gmail.com>
2020-09-28 15:21:16 +0300

    Locking down queries

--- graphQL-prisma/src/resolvers/Query.js
+++ graphQL-prisma/src/resolvers/Query.js
@@ -1,12 +1,14 @@
+import getUserId from '../utils/getUserId'
+
 const Query = {
     users(parent, args, { prisma }, info) {
         const opArgs = {}
-
-        if(args.query) {
+        
+        if (args.query) {
             opArgs.where = {
                 OR: [{
                     name_contains: args.query
-                },{
+                }, {
                     email_contains: args.query
                 }]
             }
@@ -14,17 +16,39 @@ const Query = {
 
         return prisma.query.users(opArgs, info)
     },
-    posts(parent, args, { prisma }, info) {
-        const opArgs = {}
-
-        if(args.query) {
-            opArgs.where = {
-                OR: [{
-                    title_contains: args.query
-                },{
-                    body_contains: args.query
-                }]
+    myPosts(parent, args, { prisma, request }, info) {
+        const userId = getUserId(request)
+        const opArgs = {
+            where: {
+                author: {
+                    id: userId
+                }
             }
+        }
+
+        if (args.query) {
+            opArgs.where.OR = [{
+                title_contains: args.query
+            }, {
+                body_contains: args.query
+            }]
+        }
+
+        return prisma.query.posts(opArgs, info)
+    },
+    posts(parent, args, { prisma }, info) {
+        const opArgs = {
+            where: {
+                published: true
+            }
+        }
+
+        if (args.query) {
+            opArgs.where.OR = [{
+                title_contains: args.query
+            }, {
+                body_contains: args.query
+            }]
         }
 
         return prisma.query.posts(opArgs, info)
@@ -32,20 +56,36 @@ const Query = {
     comments(parent, args, { prisma }, info) {
         return prisma.query.comments(null, info)
     },
-    me() {
-        return {
-            id: '123098',
-            name: 'Mike',
-            email: 'mike@example.com'
-        }
+    me(parent, args, { prisma, request }, info) {
+        const userId = getUserId(request)
+        
+        return prisma.query.user({
+            where: {
+                id: userId
+            }
+        })
     },
-    post() {
-        return {
-            id: '092',
-            title: 'GraphQL 101',
-            body: '',
-            published: false
+    async post(parent, args, { prisma, request }, info) {
+        const userId = getUserId(request, false)
+
+        const posts = await prisma.query.posts({
+            where: {
+                id: args.id,
+                OR: [{
+                    published: true
+                }, {
+                    author: {
+                        id: userId
+                    }
+                }]
+            }
+        }, info)
+
+        if (posts.length === 0) {
+            throw new Error('Post not found')
         }
+
+        return posts[0]
     }
 }
 
