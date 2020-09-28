53c52a5dd0a9c20c2635bf1776798e12ee4e315e
petros <pit.trak@gmail.com>
2020-09-28 18:59:24 +0300

    Token expiration

--- graphQL-prisma/src/resolvers/Mutation.js
+++ graphQL-prisma/src/resolvers/Mutation.js
@@ -1,6 +1,6 @@
 import bcrypt, { compare } from 'bcrypt'
-import jwt from 'jsonwebtoken'
 import getUserId from '../utils/getUserId'
+import generateToken from '../utils/generateToken'
 
 const Mutation = {
     async createUser(parent, args, { prisma }, info) {
@@ -20,7 +20,7 @@ const Mutation = {
 
         return {
             user,
-            token: jwt.sign({ userId: user.id }, 'thisisasecret')
+            token: generateToken(user.id)
         }
     },
     async login(parent, args, { prisma }, info) {
@@ -42,7 +42,7 @@ const Mutation = {
 
         return {
             user,
-            token: jwt.sign({userId: user.id}, 'thisisasecret')
+            token: generateToken(user.id)
         }
     },
     async deleteUser(parent, args, { prisma, request }, info) {
